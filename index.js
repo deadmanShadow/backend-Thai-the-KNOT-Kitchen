1a0465ee03d3a2ef97032cef630600ce2d71e49b
thatUltimateJSDev <raihanshamil33@gmail.com>
2023-09-25 01:21:42 +0600

    modified

--- index.js
+++ index.js
@@ -1,109 +1,66 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ObjectId } = require('mongodb');
+
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8vqvwys.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8vqvwys.mongodb.net/knotDb?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect to MongoDB
     await client.connect();
+    const db = client.db();
 
-    const usersCollection = client.db("bistroDb").collection("users");
-    const menuCollection = client.db("knotDb").collection("menu");
-    const reviewCollection = client.db("knotDb").collection("reviews");
-    const cartCollection = client.db("knotDb").collection("carts");
+    // Define your collections
+    const usersCollection = db.collection("users");
+    const menuCollection = db.collection("menu");
+    const reviewCollection = db.collection("reviews");
+    const cartCollection = db.collection("carts");
 
-    // users related apis
-    app.get('/users', async (req, res) => {
-      const result = await usersCollection.find().toArray();
-      res.send(result);
-    });
+    // REST API routes
 
+    // Create a new user
     app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = { email: user.email }
+      const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
 
       if (existingUser) {
-        return res.send({ message: 'user already exists' })
+        return res.status(400).json({ message: 'User already exists' });
       }
 
       const result = await usersCollection.insertOne(user);
-      res.send(result);
+      res.status(201).json(result.ops[0]);
     });
 
-    // menu apis
-    app.get('/menu', async (req, res) => {
-      const result = await menuCollection.find().toArray();
-      res.send(result);
-    })
+    // Retrieve all users
+    app.get('/users', async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.json(users);
+    });
 
-    // review apis
-    app.get('/reviews', async (req, res) => {
-      const result = await reviewCollection.find().toArray();
-      res.send(result);
-    })
+    // Define other routes for menu, reviews, and carts as needed
 
-    // carts collection apis
-    app.get('/carts', async (req, res) => {
-      const email = req.query.email;
-      if (!email) {
-        res.send([]);
-      }
-      const query = { email: email };
-      const result = await cartCollection.find(query).toArray();
-      res.send(result);
-    })
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
 
-    app.post('/carts', async (req, res) => {
-      const item = req.body;
-      console.log(item);
-      const result = await cartCollection.insertOne(item);
-      res.send(result);
-    })
-
-    // items delete
-    app.delete('/carts/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await cartCollection.deleteOne(query);
-      res.send(result);
-    })
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+    // Ensure that the client will close when you finish/error
     // await client.close();
   }
 }
-run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Knot is running');
-})
-
-app.listen(port, () => {
-  console.log(`KNOT is running on port ${port}`);
-})
+run().catch(console.error);
