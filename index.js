e1556cf03b839ad6b5856a0c1d606e410959fe09
thatUltimateJSDev <raihanshamil33@gmail.com>
2023-09-24 11:25:16 +0600

    query added

--- index.js
+++ index.js
@@ -45,7 +45,17 @@ async function run() {
         res.send(result);
     })
 
-    // carts collection
+    // carts collection apis
+    app.get('/carts', async(req,res) => {
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
     app.post('/carts', async (req,res) => {
       const item = req.body;
       console.log(item);
